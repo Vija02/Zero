f2b9b5950c524775aa082dfe50c4348198859dcd
Michael Salim <salim_michaelmi@yahoo.com>
2025-12-01 22:58:05 +0000

    Feat: Some pb functions

---
+++ frontend/src/api/usePbQueries.ts
@@ -0,0 +1,95 @@
+import type {
+	ListResult,
+	RecordFullListOptions,
+	RecordListOptions,
+	RecordOptions,
+} from "pocketbase"
+import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import { usePbCollection } from "./usePocketBase"
+import { CollectionResponses } from "@/pocketbase-types"
+
+/**
+ * Use this hook to fetch a single record from a PocketBase collection.
+ *
+ * @param collectionId
+ * @param id
+ * @param options
+ * @returns
+ */
+export function usePbOne<T extends keyof CollectionResponses>(
+	collectionId: T,
+	id: string,
+	options: RecordOptions = {},
+): UseQueryResult<CollectionResponses[T], Error> {
+	const collection = usePbCollection(collectionId)
+
+	return useQuery({
+		queryKey: [collectionId, id, options],
+		queryFn: async () => await collection.getOne(id, options),
+	}) as UseQueryResult<CollectionResponses[T], Error>
+}
+
+/**
+ * Use this hook to fetch a list of records from a PocketBase collection.
+ *
+ * @param collectionId
+ * @param props
+ * @returns
+ */
+export function usePbList<T extends keyof CollectionResponses>(
+	collectionId: T,
+	props: {
+		page?: number
+		perPage?: number
+	} & RecordListOptions = {},
+): UseQueryResult<ListResult<CollectionResponses[T]>, Error> {
+	const collection = usePbCollection(collectionId)
+
+	return useQuery({
+		queryKey: [collectionId, props],
+		queryFn: async () => {
+			const { page, perPage, ...options } = props
+			return await collection.getList(page, perPage, options)
+		},
+	}) as UseQueryResult<ListResult<CollectionResponses[T]>, Error>
+}
+
+/**
+ * Use this hook to fetch the first record from a PocketBase collection based on a filter.
+ *
+ * @param collectionId
+ * @param filter
+ * @param options
+ * @returns
+ */
+export function usePbFirst<T extends keyof CollectionResponses>(
+	collectionId: T,
+	filter: string,
+	options: RecordListOptions = {},
+) {
+	const collection = usePbCollection(collectionId)
+
+	return useQuery({
+		queryKey: [collectionId, filter, options],
+		queryFn: async () => await collection.getFirstListItem(filter, options),
+	}) as UseQueryResult<CollectionResponses[T], Error>
+}
+
+/**
+ * Use this hook to fetch the full list of records from a PocketBase collection.
+ *
+ * @param collectionId
+ * @param options
+ * @returns
+ */
+export function usePbFullList<T extends keyof CollectionResponses>(
+	collectionId: T,
+	options: RecordFullListOptions = {},
+): UseQueryResult<CollectionResponses[T][], Error> {
+	const collection = usePbCollection(collectionId)
+
+	return useQuery({
+		queryKey: [collectionId, options],
+		queryFn: async () => await collection.getFullList(options),
+	}) as UseQueryResult<CollectionResponses[T][], Error>
+}
--- frontend/src/api/usePocketBase.ts
+++ frontend/src/api/usePocketBase.ts
@@ -1,6 +1,12 @@
-import { TypedPocketBase } from "@/pocketbase-types"
+import { CollectionResponses, TypedPocketBase } from "@/pocketbase-types"
+import { useMemo } from "react"
 import { usePocketBase as usePocketBaseRaw } from "use-pocketbase"
 
 export function usePocketBase() {
 	return usePocketBaseRaw() as TypedPocketBase
+}
+
+export function usePbCollection(collection: keyof CollectionResponses) {
+	const pb = usePocketBase()
+	return useMemo(() => pb.collection(collection), [pb, collection])
 }
