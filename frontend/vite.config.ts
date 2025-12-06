6d3ba635cb0c9d8a33dadbee7d0186c9b183cdaa
Michael Salim <salim_michaelmi@yahoo.com>
2025-12-06 00:49:15 +0000

    Chore: PWA ignore _

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
 		VitePWA({
 			registerType: "autoUpdate",
 			includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+			workbox: {
+				navigateFallbackDenylist: [/^\/_/],
+			},
 			manifest: {
 				name: "Zero",
 				short_name: "Zero",
